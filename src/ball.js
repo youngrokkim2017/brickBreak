434c5ad5677516a96ee77d23f3c4eff4a18106db
Patrick <patrick.yrkim@gmail.com>
2020-01-30 13:18:24 -0800

    Add collision detection for ball with paddle

--- src/ball.js
+++ src/ball.js
@@ -15,6 +15,8 @@ class Ball {
         this.size = 16;
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
+
+        this.game = game;
     }
 
     draw(ctx) {
@@ -22,15 +24,31 @@ class Ball {
     }
 
     update(deltaTime) {
+        // console.log(this.game.paddle.position.x);
+
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
 
+        // checks if it hits the wall on left or right
         if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
             this.speed.x = -this.speed.x;
         }
 
+        // checks if it hits the wall on top or bottom
         if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
             this.speed.y = -this.speed.y;
+        }
+
+        // checks if collision with paddle
+        let bottomOfBall = this.position.y + this.size;
+        let topOfPaddle = this.game.paddle.position.y;
+
+        let leftSideOfPaddle = this.game.paddle.position.x;
+        let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+        if (bottomOfBall >= topOfPaddle && this.position.x >= leftSideOfPaddle && this.position.x + this.size <= rightSideOfPaddle) {
+            this.speed.y = -this.speed.y;
+            this.position.y = this.game.paddle.position.y - this.size;
         }
     }
 }
